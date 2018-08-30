8eeb849ae4edd6a105c6bc12de08e3b9e5d8d517
Minh Nguyen <minhnguyenphuonghoang@gmail.com>
2018-08-30 10:44:35 +0700

    modify test case + add verification points

--- AgencyRevolution/Object_Repositories/accounts.py
+++ AgencyRevolution/Object_Repositories/accounts.py
@@ -6,6 +6,7 @@ Accounts_button_with_account = '//header[text()="<account>"]/following-sibling::
 Accounts_show_more = '//div[@class="remaining-link"]'
 Accounts_link_text = '//div[text()="<text>"]'
 
+Accounts_Upcoming_Renewals_Filter_title = '//h3[text()="Accounts with upcoming renewals"]'
 Accounts_Upcoming_Renewals_Filter_days = '//*[text()="Accounts with upcoming renewals"]/following-sibling::div[1]//span[3]'
 Accounts_Upcoming_Renewals_Filter_carrier = ''
 Accounts_Upcoming_Renewals_Filter_policies = ''
@@ -15,9 +16,9 @@ Accounts_Upcoming_Renewals_days = '//input[@type="number"]'
 Accounts_send = '//button[contains(., "SEND")]'
 Accounts_Send_send_one_email = '//div[text()="Send one email"]/following-sibling::div//button'
 
-
-Accounts_Message_name = '//input'
-Accounts_Message_name = '//input'
-
+Accounts_Message_name = '//div[contains(@class, "naming")]//input'
+Accounts_Message_Naming_continue = '//div[contains(@class, "naming")]//button[.="Continue"]'
+Accounts_Message_Filter_Audience_continue = '//div[contains(@class, "filter-audience")]//button[.="Continue"]'
+Accounts_Message_Preview_continue = '//div[contains(@class, "preview")]//button[.="Continue"]'
 
 Accounts_Message_Library_button_with_card_name = '//div[contains(@class,"card")][contains(., "<card_name>")]//span[text()="<button_name>"]'
--- AgencyRevolution/Object_Repositories/marketing_dashboard.py
+++ AgencyRevolution/Object_Repositories/marketing_dashboard.py
@@ -3,7 +3,7 @@
 # Description: To store all elements' locators on Home/Marketing Dashboard screen
 
 Marketing_Dashboard_title = '//div[text()="Marketing Dashboard"]'
-Marketing_Dashboard_title_template = '//div[text()="<text>"]'
+Marketing_Dashboard_title_template = '//div[normalize-space(text())="<text>"]'
 Marketing_Dashboard_Navigation_button = '//span[text()="<text>"]'
 Marketing_Dashboard_Navigation_dashboard = ''
 Marketing_Dashboard_Navigation_accounts = '//span[text()="Accounts"]'
